a5c3df96d773e4ed42ab505a0ddf075654e34804
koen2312 <64638027+koen2312@users.noreply.github.com>
2022-04-14 21:48:55 +0200

    api.js geeft nu de goeie tijd weer
    tijd is nu Europees van plaats Amerikaans

--- api.js
+++ api.js
@@ -1,12 +1,13 @@
 $("#test").click(function() {
     $.get(
-    "http://ergast.com/api/f1/2022.json",
-        function(data){
-            for(var i = data.MRData.RaceTable.Races.length -1;i >=0; i--){
-                var Race = "<td>"+ data.MRData.RaceTable.Races[i].round +"</td>";
-                var Name = "<td>"+ data.MRData.RaceTable.Races[i].raceName +"</td>";
-                var Date = "<td>"+ data.MRData.RaceTable.Races[i].date +"</td>";
-                var Time = "<td>"+ data.MRData.RaceTable.Races[i].time +"</td>";
+    "http://ergast.com/api/f1/current.json",
+        function(races){
+            for(var i = races.MRData.RaceTable.Races.length -1;i >=0; i--){
+                //ophalen van data van de api af en er td om zetten
+                var Race = "<td>"+ races.MRData.RaceTable.Races[i].round +"</td>";
+                var Name = "<td>"+ races.MRData.RaceTable.Races[i].raceName +"</td>";
+                var Date = "<td>"+ races.MRData.RaceTable.Races[i].date +"</td>";
+                var Timeraw = races.MRData.RaceTable.Races[i].time;
                 
                 Race +=$("#trrace").html();
                 $("#trrace").html(Race);
@@ -14,15 +15,24 @@ $("#test").click(function() {
                 Name +=$("#trname").html();
                 $("#trname").html(Name);
 
+                //tijd naar europees zetten dus +6 uur
+                var Timeraw = String(Timeraw);
+                var timefront = Timeraw.substr(0, 2);
+                //eerste gedeelde van de tijd pakken
+                var timeback = Timeraw.substr(2, 3);
+
+                var timefront = parseInt(timefront);
+                var Timeplus = timefront + 6;
+                //eerste en laatste gedeelde van de tijd bij elkaar zetten
+                var Timeplus = "<td>"+ Timeplus +timeback +"</td>";
+
+                Timeplus +=$("#trtime").html();
+                $("#trtime").html(Timeplus);
+
                 Date +=$("#trdate").html();
                 $("#trdate").html(Date);
-
-                Time +=$("#trtime").html();
-                $("#trtime").html(Time);
-            };
-                
-        }
-        
+            };       
+        }        
     )
 });
 
