f5ce0c10beec3bdb4972f1e08348aa06b7d60fee
koen2312 <64638027+koen2312@users.noreply.github.com>
2022-04-14 13:34:59 +0200

    api met tabel gemaakt
    table met bootstrap api nog wel een de verkeerde volgorde

--- api.js
+++ api.js
@@ -1,14 +1,30 @@
 $( "#test" ).click(function() {
-    var race = {
-        "url": "http://ergast.com/api/f1/2022.json",
-        "method": "GET",
-        "timeout": 0,
-    };
-      
-    $.ajax(race).done(function (response) {
-        var html = "<h1>" + MRData.Racetable.Race + "</h1>";
-        html += $('race').html();
-        html += $("#race").html(html);
-        console.log(response);
-    });
+    $.get(
+    "http://ergast.com/api/f1/2022.json",
+        function(data){
+            var race = data.MRData.RaceTable.Races;
+            for(let i = 0; i < data.MRData.RaceTable.Races.length; i++){
+                var race = data.MRData.RaceTable.Races[i];
+                var Race = "<td>"+ data.MRData.RaceTable.Races[i].round +"</td>";
+                var Name = "<td>"+ data.MRData.RaceTable.Races[i].raceName +"</td>";
+                var Date = "<td>"+ data.MRData.RaceTable.Races[i].date +"</td>";
+                var Time = "<td>"+ data.MRData.RaceTable.Races[i].time +"</td>";
+                
+                Race +=$("#trrace").html();
+                $("#trrace").html(Race);
+
+                Name +=$("#trname").html();
+                $("#trname").html(Name);
+
+                Date +=$("#trdate").html();
+                $("#trdate").html(Date);
+
+                Time +=$("#trtime").html();
+                $("#trtime").html(Time);
+            };
+                
+        }
+        
+    )
 });
+
--- index.php
+++ index.php
@@ -2,12 +2,20 @@
     <head>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="api.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
         <button id="test">test</button>
-        <div id="race">
-
-        </div>
+        <table class="table table-striped">
+        <tr id="trrace">
+        </tr>
+        <tr id="trname">
+        </tr>
+        <tr id="trtime">
+        </tr>
+        <tr id="trdate">
+        </tr>
+        </table>
     </body>
     <script src="api.js"></script>
 </html>
