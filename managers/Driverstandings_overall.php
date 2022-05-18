9eb809046c63b2108202ffd6b32a4f186786cac4
Koen Herrebrugh <koen.herrebrugh@gmail.com>
2022-05-18 15:24:34 +0200

    Driver Manager

--- managers/Driverstandings_overall.php
+++ managers/Driverstandings_overall.php
@@ -1,15 +1,16 @@
 <?php
     class Driverstandings_overall{
         public static function select(){
+            global $con;
 
         }
         public static function insert(){
-
-            $data = file_get_contents('http://ergast.com/api/f1/current/last/results.json');
+            global $con;
+            $data = file_get_contents('http://ergast.com/api/f1/2022/5/driverStandings.json');
             $jsonObject = json_decode($data);
             $jsonArray = $jsonObject->MRData->RaceTable->Races[0]->QualifyingResults;
 
-
+            
         }
     }
 
