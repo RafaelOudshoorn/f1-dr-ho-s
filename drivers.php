24b91bed4af10620592855fe28ef8abfd81495b6
Axenator <90309465+Axenator@users.noreply.github.com>
2022-05-18 18:10:57 +0200

    made temp header, added driver page

--- drivers.php
+++ drivers.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/admin.css">
+        <link rel="stylesheet" href="css/drivers.css">
         <link rel="stylesheet" href="css/style.css">
-        <title>Document</title>
+        <title>F1</title>
+        <link rel="icon" href="images/logo.png">
     </head>
-    <body>  
+    <body>
         <header>
             <a href="index.php">
                 <img src="images/logo.png" alt="logo" class="logo">
--- index.php
+++ index.php
@@ -7,7 +7,18 @@
     </head>
     <body>
         <header>
-            
+            <a href="index.php">
+                <img src="images/logo.png" alt="logo" class="logo">
+            </a>
+            <nav>
+                <a href="admin.php">Admin</a>
+                <a href="scoreboard.php">Scorebord</a>
+                <a href="drivers.php">Coureurs</a>
+                <img src="" alt="PFP">
+                <a href="">
+                    <img src="" alt="logout">
+                </a>
+            </nav>
         </header>
         <main>
         
