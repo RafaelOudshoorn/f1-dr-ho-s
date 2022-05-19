b7f3d8921f6596d2b7997c15934dd358bdc5fa64
Timguse <tguse@casema.nl>
2022-05-19 12:14:00 +0200

    a

--- profile.php
+++ profile.php
@@ -1,28 +1,16 @@
 <html>
     <head>
-        <?php
-            include "include/head.php";
-        ?>
+        <?php include "include/head.php";?>
+        <link rel="stylesheet" href="css/profile.css"/>
     </head>
     <body>
         <header>
-            <?php
-                include "include/header.php";
-            ?>
+            <?php include "include/header.php";?>
         </header>
         <main>
-
-        </main>
-        <footer>
-            <?php
-                include "include/footer.php";
-            ?>
-        </footer>
-    </body>
-</html>
         <?php
-            echo "<div style=\"width:150px;height:150px;border:solid 5px #28282B;border-radius:50%;overflow:hidden;background:#28282B;\">";
-                echo "<img src=\"profile/" . $pf->profile_picture . "\" style=\"height:150px;display:block;margin-left:auto;margin-right:auto;\">";
+            echo "<div class=\"profielAfbeeldingKader\">";
+            echo "<img src=\"profile/" . $pf->profile_picture . "\" class=\"profielAfbeelding\" >";
             echo "</div>";
         ?>
         <br/>
@@ -35,7 +23,14 @@
                 );
             }
             echo "<form method=\"POST\" enctype=\"multipart/form-data\">";
-                echo "<input type=\"file\" name=\"file\"><br/>";
-                echo "<input type=\"submit\" name=\"cPF\" value=\"change profile picture\">";
+            echo "<input type=\"file\" name=\"file\" class=\"buttonChooseFile\"><br/>";
+            echo "<input type=\"submit\" name=\"cPF\" value=\"change profile picture\">";
             echo "</form>";
         ?>
+        </main>
+        <footer>
+            <?php include "include/footer.php";?>
+        </footer>
+    </body>
+</html>
+        
