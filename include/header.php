d0fd7742d8221d4b670822f77b42d527eeaf9773
Axenator <90309465+Axenator@users.noreply.github.com>
2022-05-20 13:17:14 +0200

    forgor

--- include/header.php
+++ include/header.php
@@ -2,13 +2,6 @@
     <img src="images/logo.png" alt="logo" class="logo">
 </a>
 <nav>
-    <?php
-        if($_SESSION["is_admin"] == 0){
-
-        }else{
-            echo "<a href=\"admin\">Admin</a>";
-        }
-    ?>
     <a href="scoreboard">Scorebord</a>
     <a href="drivers">Coureurs</a>
     <a href="profile.php">
