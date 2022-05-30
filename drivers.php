fa669117f09f1af233c163ad6d711a4fae90cc65
Koen Herrebrugh <koen.herrebrugh@gmail.com>
2022-05-30 13:29:54 +0200

    Update Drivers.php

--- drivers.php
+++ drivers.php
@@ -1,13 +1,36 @@
 <html>
     <head>
-        <?php include "include/head.php"; ?>
+        <?php include "include/head.php"; 
+        $drivers = DriverManager::select();
+        ?>
     </head>
     <body>
         <header>
             <?php include "include/header.php"; ?>
         </header>
         <main>
-
+            <table class="table table-striped">
+                <thead class="table-dark">
+                    <th>Race name</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>nationality</th>
+                    <th>permanentNumber</th>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($drivers as $driver){
+                            echo "<tr>";
+                            echo "<td>$driver->drivername</td>";
+                            echo "<td>$driver->givenName</td>";
+                            echo "<td>$driver->familyName</td>";
+                            echo "<td>$driver->nationality</td>";
+                            echo "<td>$driver->permanentNumber</td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </tbody>
+            </table>
         </main>
         <footer>
             <?php include "include/footer.php"; ?>
