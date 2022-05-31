73471485e40fc3edb133bdf0bda29d37f660788b
Axenator <90309465+Axenator@users.noreply.github.com>
2022-05-31 11:48:59 +0200

    base scoreboard

--- scoreboard.php
+++ scoreboard.php
@@ -7,7 +7,18 @@
             <?php include "include/header.php"; ?>
         </header>
         <main>
-
+            <table class="table table-striped">
+                <thead class="table-dark">
+                    <th>Race name</th>
+                    <th>Country</th>
+                    <th>Race Date</th>
+                    <th>Race Time</th>
+                    <th></th>
+                </thead>
+                    <tbody>
+                       
+                    </tbody>
+            </table>
         </main>
         <footer>
             <?php include "include/footer.php"; ?>
