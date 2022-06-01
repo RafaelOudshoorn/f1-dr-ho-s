022e9e692382e88daeefd2ec0aaae22326120939
Axenator <90309465+Axenator@users.noreply.github.com>
2022-06-01 14:32:42 +0200

    h

--- include/scripts.js
+++ include/scripts.js
@@ -1,3 +1,19 @@
 function toTop() {
     document.body.scrollTop = 0;
+};
+function scoreSelect(side){
+    var S1 = document.getElementById("s1");
+    var S2 = document.getElementById("s2");
+    var S3 = document.getElementById("s3");
+    var S1c = getComputedStyle(S1);
+    var S3c = getComputedStyle(S3);
+    if(side == "l" && S1c.backgroundColor != "rgb(40, 40, 43)"){
+        S1.style.backgroundColor = "#28282B";
+        S2.style.backgroundColor = "#28282B";
+        S3.style.backgroundColor = "#D2042D";
+    } else if(side == "r" && S3c.backgroundColor != "rgb(40, 40, 43)"){
+        S1.style.backgroundColor = "#D2042D";
+        S2.style.backgroundColor = "#28282B";
+        S3.style.backgroundColor = "#28282B";
+    }
 }
--- scoreboard.php
+++ scoreboard.php
@@ -1,22 +1,32 @@
 <html>
     <head>
         <?php include "include/head.php"; ?>
+        <link rel="stylesheet" href="css/scoreboard.css">
     </head>
     <body>
         <header>
             <?php include "include/header.php"; ?>
         </header>
         <main>
+            <div class="select">
+                <div id="s1" onclick="scoreSelect('l')">
+                    Users
+                </div>
+                <div id="s2"></div>
+                <div id="s3" onclick="scoreSelect('r')">
+                    Standings
+                </div>
+            </div>
             <table class="table table-striped">
                 <thead class="table-dark">
-                    <th>Race name</th>
-                    <th>Country</th>
-                    <th>Race Date</th>
-                    <th>Race Time</th>
-                    <th></th>
+                    <th>Username</th>
+                    <th>Points</th>
                 </thead>
                     <tbody>
-                       
+                       <tr>
+                           <td>xd</td>
+                           <td>xd</td>
+                       </tr>
                     </tbody>
             </table>
         </main>
