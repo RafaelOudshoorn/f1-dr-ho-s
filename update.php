c8625e65e95fdaca714fd0a0c298015aaca59e23
koen2312 <64638027+koen2312@users.noreply.github.com>
2022-06-03 18:34:08 +0200

    update time

--- update.php
+++ update.php
@@ -1,4 +1,8 @@
 <?php
+
+    $now = date("Y-m-d",(strtotime("now")));
+    $sun = date("Y-m-d",(strtotime("this sun")));
+    $mon = date("Y-m-d",(strtotime("this mon")));
     require_once "include/autoloader.php";
     $nextRace = RaceManager::AankomendeRace();
     $qualirace = RaceManager::Qualirace();
@@ -9,9 +13,45 @@
 
     $nextRacearray = $nextRace->round - 1;
     $qualiRacearray = $qualirace->round - 1;
-    
+
+    if($now == $mon){
+        if(strval($nextRacearray) !== $race[1]->round){
+            if($over[1]->round !== strval($nextRacearray)){
+                echo "Driverstandings update";
+                Driverstandings_overall::update();
+            }else{
+                echo "Driverstandings heeft al goede data";
+            };
+            if($last[1]->round !== strval($nextRacearray)){
+                echo "Lastrace update";
+                LastraceManager::update();
+            }else{
+                echo "Lastrace heeft al goede data";
+            };
+        }else{
+            echo "Race database heeft geen round";
+        };
+    }else{
+        echo "het is niet maandag";
+    }
+
+    if($now == $sun){
+        echo "update quali";
+        if(strval($nextRacearray) !== $race[1]->round){
+            if($quali[1]->round !== strval($qualiRacearray)){
+                echo "Qualifying update";
+                QualifyingManager::update($qualiRacearray);
+            }else{
+                echo "Qualifying heeft al goede data";
+            }
+        }else{
+            echo "Race database heeft geen round";
+        };
+    }else{
+        echo "het is niet zondag";
+    }
+
     if(strval($nextRacearray) !== $race[1]->round){
-        
         if($quali[1]->round !== strval($qualiRacearray)){
             echo "Qualifying update";
             QualifyingManager::update($qualiRacearray);
